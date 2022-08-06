427bd00ccadeb4d6c5b514271cb104c7129003b9
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-08-06 08:35:34 +0700

    Initial commit

---
+++ SK-Lib-stdio.h-puts-Bagian6__C/Source.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+/*
+	Source by AlphaCodingSkills (https://www.alphacodingskills.com)
+	Modified For Learn by RK
+	I.D.E : VS2022
+*/
+
+int main() {
+	char str[50] = "Halo Manusia!";
+
+	puts(str);
+
+	_getch();
+	return 0;
+}
